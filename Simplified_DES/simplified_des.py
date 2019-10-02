e0c62d922de9f23640ddb108379474d1c06f80f2
Spaceship <home@Spaceships-MacBook-Pro.local>
2019-10-02 08:51:51 -0500

    new

--- Simplified_DES/simplified_des.py
+++ Simplified_DES/simplified_des.py
@@ -24,29 +24,27 @@ def s_box(left, right):
 #---------------------------------------------------------------
 # Main 
 
-# key and cipher string
 key = BinNumber('0111111101','bin', 10)
 cipher = BinNumber('10100010', 'bin', 8)
 
 # Initial Permutation
 permutated_cipher = cipher.transformation(glo_var.IP)
-print(permutated_cipher)
 
-# Split the Initial cipher
+# Split the Initial cipher into two halves(left, right)
 IP_left, IP_right = permutated_cipher.split()
 print(IP_left, IP_right)
 
 # Permutate the right part
 permutated_IP_right = IP_right.transformation(glo_var.E)
-print(permutated_IP_right)
 
+#============================================
 # Key Generation
+
 permutated_key = key.transformation(glo_var.P10)
 left, right = permutated_key.split()
 
 # Bit Shift (1)
 left, right = left << 1, right << 1
-
 combined = left + right
 k1 = combined.transformation(glo_var.P8)
 print("K1: {}".format(k1))
@@ -57,16 +55,21 @@ combined = left + right
 k2 = combined.transformation(glo_var.P8)
 print("K2: {}".format(k2))
 
-# 'permutated_IP_right' XOR 'K2'
-xor_IP_right = (permutated_IP_right) ^ (k2)
+#===========================================PART I
+
+# Expand the IP_right
+ex_IP_right = permutated_IP_right.transformation(glo_var.E)
+
+# 'ex_IP_right' XOR 'K2'
+xor_IP_right = (ex_IP_right) ^ (k2)
 print(xor_IP_right)
 
 # split xor_IP_right
-E_IP_left, E_IP_right = xor_IP_right.split()
-print(E_IP_left, E_IP_right)
+k2_IP_left, k2_IP_right = xor_IP_right.split()
+print(k2_IP_left, k2_IP_right)
 
 # S box(k2)
-s_result_k2 = s_box(E_IP_left, E_IP_right)
+s_result_k2 = s_box(k2_IP_left, k2_IP_right)
 
 # P4 permutation(k2)
 permutated_p4_k2 = s_result_k2.transformation(glo_var.P4)
@@ -76,31 +79,35 @@ xor_P4 = (IP_left) ^ permutated_p4_k2
 print("xor_P4:{}".format(xor_P4))
 
 # SW function
-sw = xor_P4 + IP_right
+sw =  IP_right + xor_P4
+print("-----------------------")
 print("sw:{}".format(sw))
+print("-----------------------")
 
-# permutate the right part again
-permutated_sw_right = IP_right.transformation(glo_var.E)
+
+#=============================================PART II
+# Expand the right part again
+ex_sw_right = xor_P4.transformation(glo_var.E)
 
 # 'permutated_sw_right' XOR 'K1'
-xor_sw_right = (permutated_sw_right) ^ (k1)
-print('xor_sw_right:{}'.format(xor_sw_right))
+xor_sw_right = (ex_sw_right) ^ (k1)
 
-# split xor_sw_right
-E_sw_left, E_sw_right = xor_sw_right.split()
+# split xor_sw_right into two halves
+k1_sw_left, k1_sw_right = xor_sw_right.split()
 
 #S box(k1)
-s_result_k1 = s_box(E_sw_left, E_sw_right)
+s_result_k1 = s_box(k1_sw_left, k1_sw_right)
 
 # P4 permutation(k1)
 permutated_p4_k1 = s_result_k1.transformation(glo_var.P4)
 
-# 'P4 permutation(k1)' XOR 'xor_P4'
-xor_P4_K2 = (permutated_p4_k1) ^ (xor_P4)
+# 'P4 permutation(k1)' XOR 'IP_right'
+xor_P4_K2 = (permutated_p4_k1) ^ (IP_right)
 
-#result
-xor_result = xor_P4_K2 + IP_right
+# XOR result
+xor_result = xor_P4_K2 + xor_P4
 
 #Final permutation
 fp = xor_result.transformation(glo_var.IPi)
-print("the plaintext is:{}".format(fp))
+print('\n\n')
+print("the Cipher is:{}".format(fp))
