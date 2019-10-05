86f213e149a515fb6426ae4832b5c9063f96c239
Spaceship <home@Spaceships-MacBook-Pro.local>
2019-10-05 18:52:40 -0500

    files

--- Simplified_DES/simplified_des.py
+++ Simplified_DES/simplified_des.py
@@ -13,11 +13,21 @@ def s_box(left, right):
     result = BinNumber(0, 'bin', 0)
 
     for s_box, segment in zip(glo_var.S_BOX, segments):
-        col = int(segment[0] + segment[-1], 2)
-        row = int(segment[1] + segment[2], 2)
+        print("===================")
+        print("Segment: {}".format(segment))
+        row = int(segment[0] + segment[-1], 2)
+        col = int(segment[1] + segment[2], 2)
+
+        print("row:{}".format(row))
+        print("col:{}".format(col))
 
         value = s_box[row][col]
+        print("value:{}".format(value))
         result += BinNumber(value, 'dec', 2)
+    
+    print("=========================")
+    print("result:{}".format(result))
+    print("=========================")
 
     return result
 
@@ -26,16 +36,6 @@ def s_box(left, right):
 
 key = BinNumber('0111111101','bin', 10)
 cipher = BinNumber('10100010', 'bin', 8)
-
-# Initial Permutation
-permutated_cipher = cipher.transformation(glo_var.IP)
-
-# Split the Initial cipher into two halves(left, right)
-IP_left, IP_right = permutated_cipher.split()
-print(IP_left, IP_right)
-
-# Permutate the right part
-permutated_IP_right = IP_right.transformation(glo_var.E)
 
 #============================================
 # Key Generation
@@ -55,14 +55,30 @@ combined = left + right
 k2 = combined.transformation(glo_var.P8)
 print("K2: {}".format(k2))
 
+#============================================
+# DES Algorithm
+
+# Initial Permutation
+permutated_cipher = cipher.transformation(glo_var.IP)
+print("permutated_cipher:{}".format(permutated_cipher))
+
+# Split the Initial cipher into two halves(left, right)
+IP_left, IP_right = permutated_cipher.split()
+print(IP_left, IP_right)
+
+# Permutate the right part
+permutated_IP_right = IP_right.transformation(glo_var.E)
+print("e: {}".format(permutated_IP_right))
+
 #===========================================PART I
 
 # Expand the IP_right
-ex_IP_right = permutated_IP_right.transformation(glo_var.E)
+#ex_IP_right = permutated_IP_right.transformation(glo_var.E)
+#print("ex_IP_right:{}".format(ex_IP_right))
 
 # 'ex_IP_right' XOR 'K2'
-xor_IP_right = (ex_IP_right) ^ (k2)
-print(xor_IP_right)
+xor_IP_right = (permutated_IP_right) ^ (k2)
+print("a: {}".format(xor_IP_right))
 
 # split xor_IP_right
 k2_IP_left, k2_IP_right = xor_IP_right.split()
@@ -76,7 +92,6 @@ permutated_p4_k2 = s_result_k2.transformation(glo_var.P4)
 
 # IP_left XOR P4
 xor_P4 = (IP_left) ^ permutated_p4_k2
-print("xor_P4:{}".format(xor_P4))
 
 # SW function
 sw =  IP_right + xor_P4
@@ -88,26 +103,44 @@ print("-----------------------")
 #=============================================PART II
 # Expand the right part again
 ex_sw_right = xor_P4.transformation(glo_var.E)
+print("ex_sw_right is:{}".format(ex_sw_right))
 
 # 'permutated_sw_right' XOR 'K1'
 xor_sw_right = (ex_sw_right) ^ (k1)
+print("xor_sw_right is:{}".format(xor_sw_right))
 
 # split xor_sw_right into two halves
-k1_sw_left, k1_sw_right = xor_sw_right.split()
+k1_sw_s0, k1_sw_s1 = xor_sw_right.split()
 
 #S box(k1)
-s_result_k1 = s_box(k1_sw_left, k1_sw_right)
+s_result_k1 = s_box(k1_sw_s0, k1_sw_s1)
+print("s_result_k1 is {}".format(s_result_k1))
 
 # P4 permutation(k1)
 permutated_p4_k1 = s_result_k1.transformation(glo_var.P4)
+print("The permutated_p4_k1 is:{}".format(permutated_p4_k1))
 
 # 'P4 permutation(k1)' XOR 'IP_right'
 xor_P4_K2 = (permutated_p4_k1) ^ (IP_right)
 
 # XOR result
 xor_result = xor_P4_K2 + xor_P4
+print("The xor_result is:{}".format(xor_result))
 
 #Final permutation
 fp = xor_result.transformation(glo_var.IPi)
 print('\n\n')
 print("the Cipher is:{}".format(fp))
+
+# convert the binary to letters
+fp_left, fp_right = fp.split()
+
+# make the BinNumber to integer
+fp_left_int, fp_right_int = int(fp_left.value, 2), int(fp_right.value, 2)
+print(fp_left_int, fp_right_int)
+
+fp_left_letter, fp_right_letter = chr(fp_left_int + ord('A')), chr(fp_right_int + ord('A'))
+
+fp_letter = fp_left_letter + fp_right_letter
+print("the Plaintext is:{}".format(fp_letter))
+
