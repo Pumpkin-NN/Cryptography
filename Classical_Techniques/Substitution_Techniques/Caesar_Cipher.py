34f40d50611647df18b8a024c96134fdeb4bb8f4
Spaceship <home@Spaceships-MacBook-Pro.local>
2019-10-07 17:10:31 -0500

    change

--- Classical_Techniques/Substitution_Techniques/Caesar_Cipher.py
+++ Classical_Techniques/Substitution_Techniques/Caesar_Cipher.py
@@ -13,14 +13,13 @@ def caesar_cipher_decryption(key, plaintext):
 # Main Function
 if __name__== "__main__":
 
-    # Type in the plaintext
-    plaintext = input("Please type in your plaintext: ")
-    plaintext_list = plaintext.split()
-    #print(plaintext)
+    # Type in the Plain Text
+    # Convert the input to List
+    plaintext = list(input("Please type in your plaintext: "))
 
     # key = [1, 25]
     # Generate Random Key
     key = int(input("Please input the key value, and the key scale from 1 to 25: "))
 
-    caesar_cipher_encryption(key, plaintext_list)
-    caesar_cipher_decryption(key, plaintext_list)
+    caesar_cipher_encryption(key, plaintext)
+    caesar_cipher_decryption(key, plaintext)
