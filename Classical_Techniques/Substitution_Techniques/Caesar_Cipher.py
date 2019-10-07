b15a69b3815b1dcd92882b013288432106d9db39
Spaceship <home@Spaceships-MacBook-Pro.local>
2019-10-07 17:34:43 -0500

    new function decrypt

--- Classical_Techniques/Substitution_Techniques/Caesar_Cipher.py
+++ Classical_Techniques/Substitution_Techniques/Caesar_Cipher.py
@@ -7,19 +7,37 @@ def caesar_cipher_encryption(key, plaintext):
     print(result)
 
 # Define Caesar Cipher Decryption
-def caesar_cipher_decryption(key, plaintext):
-    caesar_cipher_encryption(key == -key, plaintext)
+def caesar_cipher_decryption(key, ciphertext):
+    #caesar_cipher_encryption(key == -key, ciphertext)
+    result = ''
+    for letter in ciphertext:
+        # key == -key 
+        C = (ord(letter) - key)
+        result = result + chr(C)
+    print(result)
 
 # Main Function
 if __name__== "__main__":
 
-    # Type in the Plain Text
-    # Convert the input to List
-    plaintext = list(input("Please type in your plaintext: "))
+    choice = input("Do you want to Encrypt or Decrypt? (E/D) :\n")
+    
 
-    # key = [1, 25]
-    # Generate Random Key
-    key = int(input("Please input the key value, and the key scale from 1 to 25: "))
+    
+    if(choice == 'E'):
+        # Type in the Plain Text
+        # Convert the input to List
+        plaintext = list(input("Please type in your plaintext:\n"))
+        # key = [1, 25]
+        # Generate Random Key
+        key = int(input("Please input the key value, and the key scale from 1 to 25:\n"))
+        caesar_cipher_encryption(key, plaintext)
+    else:
+        ciphertext = list(input("Please type in your ciphertext:\n"))
+        # key = [1, 25]
+        # Generate Random Key
+        key = int(input("Please input the key value, and the key scale from 1 to 25:\n"))
+        caesar_cipher_decryption(key, ciphertext)
+        
 
-    caesar_cipher_encryption(key, plaintext)
-    caesar_cipher_decryption(key, plaintext)
+    
+   
