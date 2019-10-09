bb45b7c95a794234c0d1a5e279b613d8a9e51b3a
Spaceship <home@Spaceships-MacBook-Pro.local>
2019-10-09 01:22:37 -0500

    change affine

--- Classical_Techniques/Substitution_Techniques/Monoalphabetic_Ciphers/Affine_Caesar_Cipher.py
+++ Classical_Techniques/Substitution_Techniques/Monoalphabetic_Ciphers/Affine_Caesar_Cipher.py
@@ -2,23 +2,13 @@ import random
 import math
 
 ALPHABET = tuple("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-# m = [1, 26]
-m = random.randrange(1, 26)
-
-# Find a
-while True:
-    a = random.randrange(1, 26)
-    if math.gcd(a,26) == 1 and a != m:
-        a = a
-        break
+# M, an alphabet size
+M = 26 
 
 # encrypt the message
 def affine_cipher_encryption(plaintext):
+
     
-    # 1 <= b and b <= m
-    b = m
-    print("b value:{}, a value:{}".format(b, a))
     # Initial ciphertext
     ciphertext = ''
 
@@ -28,8 +18,7 @@ def affine_cipher_encryption(plaintext):
         Mod_1 = a * (ord((''.join(letter)))-65) + b
         #print("Mod_1:{}".format(Mod_1))
             
-        # M range in [0, 25], an alphabet size
-        Mod_2 = m
+        Mod_2 = M
         #print("Mod_2:{}".format(Mod_2))
 
         # Find Ciphertext(numbers)
@@ -47,6 +36,18 @@ def affine_cipher_decryption():
     pass
 
 if __name__ == "__main__":
+
+    # Find b
+    b = random.randrange(1, 26)
+
+    # Find a
+    while True:
+        a = random.randrange(1, 26)
+        if math.gcd(a, 26) == 1 and a != M:
+            a = a
+            break
+
+    print("b value:{}, a value:{}".format(b, a))
 
     # Type in the plain text
     plaintext = list(input("Please enter the text: "))
