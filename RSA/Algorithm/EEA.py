bc527d57cc12fe0af90c512debe9b21d2c1b3a4e
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2019-11-15 15:58:49 -0600

    finish RSA algorithm

--- RSA/Algorithm/EEA.py
+++ RSA/Algorithm/EEA.py
@@ -6,20 +6,11 @@ def extended_euclidean_algorithm(a, b):
     list_t = []
     # Algorithm
     while b > 0:
-        #print("({}, {})\n".format(a, b))
-
         # Find the remainder of a, b
         r = a % b
-        #print("the r is {}".format(r))
-
-        #print("the a is {}".format(a))
-        #print("the b is {}".format(b))
-
         if r > 0:
             # The t expression
-            t = (r - (a * s)) // b 
-            #print("the s is {}".format(s))
-            #print("the t is {}".format(t))
+            t = (r - (a * s)) // b
             list_t.append(t)
             list_s.append(s)
 
@@ -28,24 +19,15 @@ def extended_euclidean_algorithm(a, b):
         if r > 0:
             # Use the remainder to be the new b
             b = r
-
-            #print("the r is {}".format(r))
-            #print("the a is {}".format(a))
-            #print("the b is {}".format(b))
         else:
             break
-        #print("--------------------------------------")
-
-    #print(list_s, list_t)
 
     # Find the coefficients s and t
     for i in range(len(list_t)):
         if i+1 < len(list_t):
             # Find the coefficient t
             t = list_t[0] + (list_t[(len(list_t)-1)] * s)
-            print("t = ", t)
-
             # Find the coefficient s
             s = list_s[i] + list_t[i] * list_t[i+1]
-            #print("s = ", s)
+        
     return t
---
+++ RSA/Algorithm/RSA.py
@@ -0,0 +1,72 @@
+"""
+Author: Nicole
+Last modifyed: 11/15 2019
+Description: the foundation of the RSA algorithm
+"""
+
+import random
+from EEA import extended_euclidean_algorithm
+
+
+# Choose two prime numbers
+P = 17
+Q = 11
+# Calculate the N
+N = P * Q
+
+
+
+# Function: find the greatest common divisor
+def gcd(a, b):
+    cds = []
+    for i in range(1, max(a, b)):
+        if a % i == 0 and b % i == 0:
+            cds.append(i)
+    gcd = cds[-1]
+    return gcd
+
+# Fuction: calculate Φ(N)
+def phi_N():
+    sum_phi_N = 0
+    for i in range(1, N):
+        if gcd(i, N) == 1:
+            sum_phi_N = sum_phi_N + 1
+    return sum_phi_N
+
+# Function: Select e, such that e is relatively prime to Φ(N)
+e_list = []
+def choose_e(val):
+    for i in range(1, phi_n):
+        if gcd(i, phi_n) == 1 and i < phi_n:
+            e_list.append(i)
+    # Randomly choose a number from the e_list
+    #return random.choice(e_list)
+    return e_list[2]
+
+# Function: Determine d, such d and e are multiplicative inverse
+def determine_d(val_1, val_2):
+    d = abs(extended_euclidean_algorithm(phi_n, e))
+    if d < phi_n:
+        return d
+    else:
+      return -1
+
+# Main Function
+if __name__== "__main__":
+    # Calculate Φ(N)
+    phi_n = phi_N()
+    print("phi_n value is: ", phi_n)
+
+    # Select e, we choose 7 in this case
+    e = choose_e(phi_n)
+    print("e value is: ", e)
+
+    # Determine d
+    d = determine_d(phi_n, e)
+    print("Calculate the d is: ", d)
+
+    # The resulting public key
+    print("The public key is: [{}, {}]".format(e, N))
+
+    # The resulting private key
+    print("The private key is: [{}, {}]".format(d, N))
