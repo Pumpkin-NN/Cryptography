0c3be05f088c9939cd20e671fd0aa72c37146a47
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2019-11-27 20:41:07 -0600

    edit

--- RSA/Algorithm/simplified_text#2.py
+++ RSA/Algorithm/simplified_text#2.py
@@ -16,20 +16,6 @@ def string2num(*List):
     #print("num:{}".format(num))
     return num
 
-def ciphertext_trigraphs(*List):
-
-    List = copy.deepcopy(*List)
-    trigraphs = []
-    for i in range(len(List)):
-        a = List[i] // (40**2)
-        r1 = List[i] - (a * 40**2)
-        b = r1 // 40
-        c = ( List[i] - (a * 40**2 + b * 40) ) % 40
-        trigraphs.append(a)
-        trigraphs.append(b)
-        trigraphs.append(c)
-    return trigraphs
-
 def num2string(*List):
     List = copy.deepcopy(*List)
     # Convert it into the text
@@ -55,7 +41,7 @@ def RSA(pub_key,n):
 
     return pri_key
 
-def ciphertext_encryption_graphs(Encrypted_Message, blocks, base):
+def transfer_graphs(Encrypted_Message, blocks, base):
     length = len(Encrypted_Message)
     segment = length // blocks
 
@@ -94,7 +80,7 @@ def ciphertext_encryption_graphs(Encrypted_Message, blocks, base):
             #               (base**5) * sub_chuncks[k+1] + \
             #               (base**6) * sub_chuncks[k] + \
 
-def plaintext_decryption_graphs(blocks, base, *List):
+def graphs_transfer(blocks, base, *List):
     List = copy.deepcopy(*List)
     plaintext_nums = []
     for i in List:
@@ -102,7 +88,7 @@ def plaintext_decryption_graphs(blocks, base, *List):
         for index in range(blocks):
             co_num = blocks -1 - index
             plaintext_num = remainder // (base ** (co_num))
-            remainder = i % (26 ** (co_num))
+            remainder = i % (base ** (co_num))
             plaintext_nums.append(plaintext_num)
     print(plaintext_nums)
     return plaintext_nums
@@ -123,14 +109,14 @@ def example1():
     message = list(message)
     print("plaintext: {}".format("".join(message)))
 
-    digraphs = ciphertext_encryption_graphs(message, 2, 40)
+    digraphs = transfer_graphs(message, 2, 40)
 
     pub_key = 179
     n = 2047
     cipher_num = rsa_algorithm(pub_key, n, digraphs)
     print(cipher_num)
 
-    encrypted_message_nums = ciphertext_trigraphs(cipher_num)
+    encrypted_message_nums = graphs_transfer(3, 40, cipher_num)
     print(encrypted_message_nums)
 
     encrypted_message = num2string(encrypted_message_nums)
@@ -145,12 +131,12 @@ def example2():
     n = 536813567
     pri_key = 201934721
     cipher_num = []
-    cipher_num = ciphertext_encryption_graphs(Encrypted_Message, 7, 26)
+    cipher_num = transfer_graphs(Encrypted_Message, 7, 26)
     #text_num = rsa_algorithm(pri_key, n, cipher_num)
     # Modular Exponentiation Calculator
     cipher_num = [20002966, 11198842, 12223445]
     print("After Modular:{}".format(cipher_num))
-    plaintext_nums = plaintext_decryption_graphs(6, 26, cipher_num)
+    plaintext_nums = graphs_transfer(6, 26, cipher_num)
 
     plaintext = num2string(plaintext_nums)
     print(plaintext)
@@ -164,12 +150,12 @@ def example3():
     n = 536813567
     pri_key = 201934721
     cipher_num = []
-    cipher_num = ciphertext_encryption_graphs(Encrypted_Message, 7, 26)
+    cipher_num = transfer_graphs(Encrypted_Message, 7, 26)
     #text_num = rsa_algorithm(pri_key, n, cipher_num)
     # Modular Exponentiation Calculator
     cipher_num = [45005201, 56094542]
     print("After Modular:{}".format(cipher_num))
-    plaintext_nums = plaintext_decryption_graphs(6, 26, cipher_num)
+    plaintext_nums = graphs_transfer(6, 26, cipher_num)
 
     plaintext = num2string(plaintext_nums)
     print(plaintext)
@@ -180,5 +166,3 @@ if __name__== "__main__":
     example2()
     example3()
     
-
-
