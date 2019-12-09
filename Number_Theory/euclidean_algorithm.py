37d0c1dc3576980c7a0f87fe1aafe6096bf717f2
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2019-12-08 20:12:26 -0600

    change

--- Number_Theory/euclidean_algorithm.py
+++ Number_Theory/euclidean_algorithm.py
@@ -15,5 +15,9 @@ result1 = euclidean_algorithm(973, 301)
 print("The result is: {}".format(result1))
 
 print("\n\nThe second test: ")
-result2 = euclidean_algorithm(27, 21)
+result2 = euclidean_algorithm(301, 973)
 print("The result is: {}".format(result2))
+
+print("\n\nThe third test: ")
+result3 = euclidean_algorithm(27, 21)
+print("The result is: {}".format(result3))
